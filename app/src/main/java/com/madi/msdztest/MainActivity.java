0b03ca55cd39219d83e8eddc0c2699fb9c67725a
PC <Salahmadi91@gmail.com>
2023-05-04 16:17:22 +0100

    2nd push

--- app/src/main/java/com/madi/msdztest/MainActivity.java
+++ app/src/main/java/com/madi/msdztest/MainActivity.java
@@ -1,14 +1,45 @@
 package com.madi.msdztest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.madi.msdztest.databinding.ActivityMainBinding;
 
+public class  MainActivity extends AppCompatActivity {
+ ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragement(new Fragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.Acceuil:
+                    replaceFragement(new Fragment() );
+                    break;
+                case R.id.Messages:
+                    replaceFragement(new MessagesFragment());
+                    break;
+                case R.id.Paramètres:
+                    replaceFragement(new SettingsFragment());
+                    break;
+
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragement(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainConstraint,fragment);
+        fragmentTransaction.commit();
     }
 }
