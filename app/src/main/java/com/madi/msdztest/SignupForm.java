418d933255f6d228ee03e7be545b811696edd2ab
PC <Salahmadi91@gmail.com>
2023-05-07 19:24:48 +0100

    added Creer un compte Ui

---
+++ app/src/main/java/com/madi/msdztest/SignupForm.java
@@ -0,0 +1,59 @@
+package com.madi.msdztest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class SignupForm extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private SignupFragmentAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_form);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager2);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Client"));
+        tabLayout.addTab(tabLayout.newTab().setText("Artisan"));
+
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new SignupFragmentAdapter(fragmentManager , getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/madi/msdztest/SignupFragmentAdapter.java
@@ -0,0 +1,27 @@
+package com.madi.msdztest;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class SignupFragmentAdapter extends FragmentStateAdapter {
+    public SignupFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position == 0){
+            return new SignupClient();
+        }
+        return new SignupArtisan();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
