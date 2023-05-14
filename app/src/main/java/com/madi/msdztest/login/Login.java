892a74d9a1ea83f73b8abefc59e38efff93833a6
PC <Salahmadi91@gmail.com>
2023-05-14 23:58:26 +0100

    update

--- app/src/main/java/com/madi/msdztest/login/Login.java
+++ app/src/main/java/com/madi/msdztest/login/Login.java
@@ -2,9 +2,13 @@ package com.madi.msdztest.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.madi.msdztest.R;
+import com.madi.msdztest.signup.SignupForm;
 
 public class Login extends AppCompatActivity {
 
@@ -12,5 +16,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView textView = findViewById(R.id.CreerCompte);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, SignupForm.class);
+                startActivity(intent);
+            }
+        });
     }
 }
